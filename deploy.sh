528f5951928e62661ff8ae247ba0125129cfd7ba
thangnc <thangnc.dev198@gmail.com>
2026-04-16 16:46:32 +0700

    fix: allow empty commit during deployment process

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd dist
 git init
 git checkout -B gh-pages
 git add -A
-git commit -m "Deploy $(date '+%Y-%m-%d %H:%M:%S')"
+git commit --allow-empty -m "Deploy $(date '+%Y-%m-%d %H:%M:%S')"
 git remote remove origin 2>/dev/null || true
 git remote add origin https://github.com/thangncdev/lpic-1.git
 git push -f origin gh-pages
